90e57ad46853df54dd97d4e1be981bd55a578c45
anthonymars <anthony.marstiller@gmail.com>
2017-05-10 14:47:13 -0400

    vid fix

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -38,10 +38,14 @@ Southern WV Enduro Trails and Info
 					<span class="mainColor">Dirt / Trail / Rock Garden only!!!</span> No street stuff... unless you are just going to the trails.</p>
 		</div>
 		<div class="col-md-6">
+			@if($d->count() > 0)
 			<h3 class="text-center"><strong>{{$d->title}} | {{ $d->user->name }}</strong></h3>
 			<div class="embed-responsive embed-responsive-16by9">
 									<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{$d->url}}"></iframe>
-							</div>
+			</div>
+			@else
+			<p>No Vids Yet</p>
+			@endif
 		</div>
 	</div>
 </div>
